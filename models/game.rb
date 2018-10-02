9be25137e32175454b2c68f0ea41c9e388fa8487
DeuterGraves <puckles@gmail.com>
2018-10-02 15:53:12 +0100

    style sheet added and game updated to name winner

--- models/game.rb
+++ models/game.rb
@@ -7,19 +7,19 @@ def self.play(weapon1,weapon2)
   case
     # rock beats scissors *2
     when weapon1 == "rock" && weapon2 == "scissors"
-      return "Rock wins!"
+      return "Player 1 wins by playing RAWK!!"
     when weapon1 == "scissors" && weapon2 == "rock"
-      return "Rock wins!"
+      return "Player 2 wins by playing RAWK!"
       # paper beats rock *2
     when weapon1 == "paper" && weapon2 == "rock"
-      return "Paper wins!"
+      return "Player 1 wins with PAPER!"
     when weapon1 == "rock" && weapon2 == "paper"
-      return "Paper wins!"
+      return "Player 2 wins with PAPER!"
       # scissors beats paper *2
     when weapon1 == "scissors" && weapon2 == "paper"
-      return "Scissors wins!"
+      return "Player 1 wins by playing SCISSORS!!!"
     when weapon1 == "paper" && weapon2 == "scissors"
-      return "Scissors wins!"
+      return "Player 2 wins by playing SCISSORS!!!"
     else
       return "Tie! Play again?"
     end
